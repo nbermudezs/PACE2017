63a76232b943dd6155c8933d8aa6448784cbf043
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2017-12-06 19:29:54 -0600

    Add methods to retrieve data in matching format for PACE

--- attribute_data.py
+++ attribute_data.py
@@ -84,6 +84,76 @@ class AttributeData:
                     continue
         print('Got attributes for ', count, ' businesses')
 
+    def user_props_as_list(self, idxs, filename='dataset/user.json'):
+        self.user_attribute_list = ['review_count', 'average_stars']
+        uids = [self.pud.idx2user_id[idx] for idx in idxs]
+        result = defaultdict(lambda: [])
+        with open(filename, 'r') as f:
+            for line in f:
+                obj = json.loads(line)
+                if obj['user_id'] in uids:
+                    idx = self.pud.user_id2idx[obj['user_id']]
+                    for attr in self.user_attribute_list:
+                        result[idx].append(obj[attr])
+        rc_values = [x[0] for x in result.values()]
+        rc_mean, rc_std = np.mean(rc_values), np.std(rc_values)
+
+        as_values = [x[1] for x in result.values()]
+        as_mean, as_std = np.mean(as_values), np.std(as_values)
+
+        return [np.concatenate(
+            (
+                binarize_attr(rc_mean, rc_std, result[idx][0]),
+                binarize_attr(as_mean, as_std, result[idx][1]))
+            ) for idx in idxs]
+
+    def business_props_as_list(self, idxs, filename='dataset/business.json'):
+        self.business_attribute_list = ['stars', 'review_count']
+        bids = [self.pud.idx2business_id[idx] for idx in idxs]
+        result = defaultdict(lambda: [])
+        with open(filename, 'r') as f:
+            for line in f:
+                obj = json.loads(line)
+                if obj['business_id'] in bids:
+                    idx = self.pud.business_id2idx[obj['business_id']]
+                    for attr in self.business_attribute_list:
+                        result[idx].append(obj[attr])
+        s_values = [x[0] for x in result.values()]
+        s_mean, s_std = np.mean(s_values), np.std(s_values)
+
+        rc_values = [x[1] for x in result.values()]
+        rc_mean, rc_std = np.mean(rc_values), np.std(rc_values)
+
+        return [np.concatenate(
+            (
+                binarize_attr(s_mean, s_std, result[idx][0]),
+                binarize_attr(rc_mean, rc_std, result[idx][1]))
+            ) for idx in idxs]
+
+
+def binarize(mean, std, values):
+    result = []
+    for attr in values:
+        result.append(binarize_attr(mean, std, attr))
+    return result
+
+def binarize_attr(mean, std, attr):
+    min_range = -10
+    max_range = 9
+    binarized = np.zeros(20)
+    for idx, i in enumerate(np.arange(min_range, max_range + 1)):
+        lower_bound = mean + i * std / 3.
+        upper_bound = mean + (i+1) * std / 3.
+
+        if i == min_range:
+            lower_bound = -float('inf')
+        elif i == max_range:
+            upper_bound = float('inf')
+        if attr > lower_bound and attr <= upper_bound:
+            binarized[idx] = 1
+            break
+    return binarized
+
 if __name__ == '__main__':
     data = AttributeData('poi-users-filtered-09112017.pkl')
     data.prepare_user()
